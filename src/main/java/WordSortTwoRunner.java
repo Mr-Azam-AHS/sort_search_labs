c1f375dabe006c4f4e17bb2fc61353dbde4899a1
Mr-Azam-AHS <eazam@andrews.esc18.net>
2026-03-26 11:55:50 -0500

    Added comment to anticipate FileNotFoundError

--- src/main/java/WordSortTwoRunner.java
+++ src/main/java/WordSortTwoRunner.java
@@ -8,17 +8,15 @@ import java.io.File;
 import java.io.IOException;
 import static java.lang.System.*;
 
-public class WordSortTwoRunner
-{
-	public static void main( String args[] ) throws IOException
-	{
-		Scanner file = new Scanner(new File("wordsorttwo.dat"));
+public class WordSortTwoRunner {
+	public static void main(String args[]) throws IOException {
+		// pathname is incomplete. Explain to students how to fix FileNotFound error.
+		Scanner file = new Scanner(new File("src/main/java/wordsorttwo.dat"));
 		int size = file.nextInt();
 		file.nextLine();
-		for(int i = 0; i<size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			String sentence = file.nextLine();
-			//instantiate a new WordSort
+			// instantiate a new WordSort
 		}
 	}
 }
